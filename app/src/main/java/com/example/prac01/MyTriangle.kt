0938ebd049127cbe6d4426940161a97ad434e988
JoInHo <zjxps2007@gmail.com>
2023-03-30 20:24:46 +0900

    Update MyTriangle.kt

--- app/src/main/java/com/example/prac01/MyTriangle.kt
+++ app/src/main/java/com/example/prac01/MyTriangle.kt
@@ -17,12 +17,14 @@ class MyTriangle {
         -0.5f, 0.5f, 0.0f,
         0.5f, 0.5f, 0.0f
     )
-//    private val triangleColors = floatArrayOf(
-//        1.0f, 1.0f, 1.0f,
-//        1.0f, 1.0f, 1.0f,
-//        1.0f, 1.0f, 1.0f,
-//
-//    )
+    private val triangleColors = floatArrayOf(
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 1.0f
+        )
 
     private val color = floatArrayOf(1.0f, 1.0f, 0.0f, 1.0f)
 
